a7793ec05e5feba81aef6fd300d366f763bb9556
Anastasia Alexadrova <anastasia.alexadrova@percona.com>
2021-03-22 13:27:54 +0200

    PSMDB-837 Release notes 4.2.13-14

--- source/conf.py
+++ source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 # The short X.Y version.
 version = '4.2'
 # The full version, including alpha/beta/rc tags.
-release = ".".join([version, '12-13'])
+release = ".".join([version, '13-14'])
 
 # General information about the project.
 product_name = 'Percona Server for MongoDB'
