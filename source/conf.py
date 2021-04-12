39dab90d57ff28a5f8c606ff9964e0f9b3a28a5c
Paul Jacobs <paul.jacobs@percona.com>
2021-04-12 15:51:31 +0300

    Ignore pesky psdom

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ sys.path.append(os.path.abspath('ext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo', 
-              'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 
+extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.ifconfig',
               'sphinx.ext.extlinks']
 
 
