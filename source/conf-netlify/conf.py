329a725d6723ab9515a55bf8684da793713f49b1
Anastasia Alexadrova <anastasia.alexandrova@percona.com>
2021-10-26 20:39:12 +0300

    added a css style for contact us block

--- source/conf-netlify/conf.py
+++ source/conf-netlify/conf.py
@@ -7,6 +7,9 @@ from conf import *
 extensions.append('sphinx_gitstamp')
 extensions.append('sphinx_copybutton')
 extensions.append('sphinx_tabs.tabs')
+html_css_files = [
+    '../_static/css/material.css',
+]
 html_sidebars['**']=['globaltoc.html', 'searchbox.html', 'localtoc.html', 'logo-text.html']
 html_theme = 'sphinx_material'
 html_theme_options = {
