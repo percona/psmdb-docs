610eafd1af03648a2936379f617d205bc32ddc0b
Anastasia Alexandrova <anastasia.alexandrova@percona.com>
2022-12-07 15:49:01 +0100

    PSMDB-1180 Release notes 6.0.3-2 (#624)

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,8 @@ master_doc = 'index'
 # The short X.Y version.
 version = '6.0'
 # The full version, including alpha/beta/rc tags.
-release = ".".join([version, '2-1'])
+
+release = ".".join([version, '3-2'])
 
 # General information about the project.
 product_name = 'Percona Server for MongoDB'
