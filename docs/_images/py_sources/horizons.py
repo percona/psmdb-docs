3355c3e78542d25268f2a5e52d2555b484a6a5f1
Anastasia Alexadrova <anastasia.alexandrova@percona.com>
2026-01-22 11:59:17 +0100

    Cleaned up diagrams script and extra folders

--- docs/_images/py_sources/horizons.py
+++ docs/_images/py_sources/horizons.py
@@ -1,36 +1,30 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.general import Client
 from diagrams.onprem.compute import Server
-from diagrams.aws.network import VPCRouter
-from diagrams.generic.network import Firewall
+from diagrams.aws.network import NLB
 from diagrams.aws.general import GenericDatabase
 
 with Diagram("Horizons workflow", show=False, filename="mongodb_horizons_diagram"):
     # External World
     external_client = Client("MongoDB Client")
 
-    with Cluster("Cloud Provider / On-premise"):
-        with Cluster("Virtual Private Cloud (VPC)"):
-            with Cluster("Public subnet"):
-                dns = Firewall("DNS\nmongo.external.mycompany.com")
-                vpc = VPCRouter("VPC Router\n52.45.100.200")
+    with Cluster("Virtual Private Cloud (VPC)"):
+            nlb = NLB("Load Balancer\nmongo.external.mycompany.com")
+            with Cluster("MongoDB Replica Set"):
+                # Create nodes in horizontal alignment
+                primary = GenericDatabase("Primary\npsmdb1.internal.net")
+                secondary1 = GenericDatabase("Secondary\npsmdb2.internal.net")
+                secondary2 = GenericDatabase("Secondary\npsmdb3.internal.net")
 
-            with Cluster("Private subnet"):
-                # MongoDB Replica Set cluster - aligned horizontally
-                with Cluster("MongoDB Replica Set"):
-                    # Create nodes in horizontal alignment
-                    primary = GenericDatabase("Primary\npsmdb1.internal.net")
-                    secondary1 = GenericDatabase("Secondary\npsmdb2.internal.net")
-                    secondary2 = GenericDatabase("Secondary\npsmdb3.internal.net")
-
-                app_server = Server("Internal app\n10.0.1.100")
+            app_server = Server("Internal app\n10.0.1.100")
 
     # External client flow
-    external_client >> Edge(label="1 - SNI: mongo.external.mycompany.com") >> dns
-    dns >> vpc
-    vpc >> Edge(label="3 - Horizons Reply") >> primary
-    primary >> Edge(label="2 - Forwards to Primary" ) >> vpc
-    vpc >> Edge(label="4 - Routes Reply") >> external_client
+    external_client >> Edge(label="4. Routes Reply") >>  nlb
+    nlb >> Edge(label="1. SNI: mongo.external.mycompany.com") >> external_client
+
+    nlb >> Edge(label="2. Forwards to Primary") >> primary
+    primary >> Edge(label="3. Horizons reply: external hostnames" ) >>  nlb
+    
 
     # Internal app flow - positioned to avoid overlap with cluster label
     app_server >> Edge(label="1 - Internal DNS (SNI: psmdb1.internal.net)") >> primary
